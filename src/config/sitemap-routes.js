98bd303aac9e917a7c348eacda5660c69bbd6cc6
giangphammm <phamthugiang2604@gmail.com>
2020-12-21 21:23:45 +0100

    update sitemaps

--- src/config/sitemap-routes.js
+++ src/config/sitemap-routes.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router';
 export default (
   <Route>
     <Route path='/bestellen' />
+    <Route path='catering' />
     <Route path='/dsgvo' />
     <Route path='/home' />
     <Route path='/impressum' />
